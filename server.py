d6dc262a452b63cda3b3e3acabb7fc03ff4c8e51
Julia Hedrick <julia.hedrick3@gmail.com>
2019-11-16 14:33:22 -0500

    sequential sever

---
+++ server.py
@@ -0,0 +1,39 @@
+from http.server import BaseHTTPRequestHandler
+import socketserver
+import time
+import json
+
+PORT        = 8080
+NUM_THREADS = 100
+
+MAP = {} # TODO: replace with map class object
+
+def requestMapUpdate(request):
+	print("in requestMapUpdate")
+	time.sleep(10)
+	return json.dumps({"res": "turn right"})
+
+
+class GetHandler(BaseHTTPRequestHandler):
+	def do_POST(self):
+		print("got post!!")
+		fields= self.headers.as_string().split('\n')
+		output = {}
+		for field in fields: 
+			kv = field.split(':')
+			if len(kv) >= 2:
+				output[kv[0]] = kv[1]
+
+		post_body = self.rfile.read(int(output['Content-Length']))
+		body = json.loads(post_body)
+		self.wfile.write(bytes(requestMapUpdate(body), "utf-8"))
+		return self.send_response(200)
+
+def main():
+	with socketserver.TCPServer(("", PORT), GetHandler) as httpd:
+		print("serving at port", PORT)
+		httpd.serve_forever()
+
+
+if __name__ == '__main__':
+	main()
