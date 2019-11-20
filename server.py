7de18a5ab13840a3d3a7654a44f600ac6edf63e6
Julia Hedrick <julia.hedrick3@gmail.com>
2019-11-20 10:30:06 -0500

    made the sever concurrent

--- server.py
+++ server.py
@@ -2,11 +2,19 @@ from http.server import BaseHTTPRequestHandler
 import socketserver
 import time
 import json
+import threading
+import selectors
+
 
 PORT        = 8080
 NUM_THREADS = 100
 
 MAP = {} # TODO: replace with map class object
+
+if hasattr(selectors, 'PollSelector'):
+    _ServerSelector = selectors.PollSelector
+else:
+    _ServerSelector = selectors.SelectSelector
 
 # TODO: remove when the map api is set
 def requestMapUpdate(request):
@@ -36,8 +44,17 @@ class GetHandler(BaseHTTPRequestHandler):
 
 def main():
 	with socketserver.TCPServer(("", PORT), GetHandler) as httpd:
-		print("serving at port", PORT)
-		httpd.serve_forever()
+		with _ServerSelector() as selector:
+			selector.register(httpd, selectors.EVENT_READ)
+
+			while True:
+				ready = selector.select()
+				if ready:
+					th = threading.Thread(target=httpd._handle_request_noblock, args=[])
+					th.start()
+
+				httpd.service_actions()
+
 
 
 if __name__ == '__main__':
